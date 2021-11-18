51130c876fd161f1759a715fbd4b05a02a1b5acc
FIRS <shuliak.lyubov@gmail.com>
2021-11-18 10:44:15 +0200

    first commit

---
+++ crwn-shop/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import {
+  googleSignInStart,
+  emailSignInStart
+} from '../../redux/user/user.actions';
+
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
+
+const  SignIn =()=> {
+  const [userCredentials, setUserCredentials] = useState({
+    email: '',
+    password: ''
+  })
+  const dispatch = useDispatch()
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+   
+    const { email, password } = userCredentials
+
+    dispatch (emailSignInStart(email, password))
+  };
+
+  const handleChange = event => {
+    const { value, name } = event.target;
+
+    setUserCredentials({...userCredentials, [name]: value });
+  };
+
+ 
+   
+    return (
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={handleChange}
+            value={userCredentials.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={userCredentials.password}
+            handleChange={handleChange}
+            label='password'
+            required
+          />
+          <ButtonsBarContainer>
+            <CustomButton type='submit' > Sign in </CustomButton>
+            <CustomButton
+              type='button'
+              onClick={() => dispatch(googleSignInStart())}
+              isGoogleSignIn
+            >
+              Sign in with Google
+            </CustomButton>
+          </ButtonsBarContainer>
+        </form>
+      </SignInContainer>
+    );
+  }
+
+
+// const mapDispatchToProps = dispatch => ({
+//   googleSignInStart: () => dispatch(googleSignInStart()),
+//   emailSignInStart: (email, password) =>
+//     dispatch(emailSignInStart({ email, password }))
+// });
+
+export default SignIn
---
+++ crwn-shop/src/pages/collection/collection.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import {  useSelector } from "react-redux";
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from './collection.styles';
+
+const CollectionPage = ()=> {
+  const {collectionId} = useParams()
+  const collection = useSelector(selectCollection(collectionId))
+  const { title, items } = collection;
+  return (
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
+  );
+};
+
+export default CollectionPage
